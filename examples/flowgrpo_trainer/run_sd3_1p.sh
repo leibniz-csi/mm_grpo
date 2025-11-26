64cd465e53af86b925ea46cb764c77295de7165b
Mike Cheung <zhtmike@gmail.com>
2025-11-26 12:51:12 +0800

    update config

--- examples/flowgrpo_trainer/run_sd3_1p.sh
+++ examples/flowgrpo_trainer/run_sd3_1p.sh
@@ -12,7 +12,7 @@ python3 -m gerl.trainer.main_flowgrpo \
     actor_rollout_ref.model.enable_gradient_checkpointing=False \
     actor_rollout_ref.model.lora_rank=32 \
     actor_rollout_ref.model.lora_alpha=64 \
-    actor_rollout_ref.actor.optim.lr=1e-4 \
+    actor_rollout_ref.actor.optim.lr=3e-4 \
     actor_rollout_ref.actor.optim.weight_decay=0.0001 \
     actor_rollout_ref.actor.ppo_mini_batch_size=4 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=8 \
