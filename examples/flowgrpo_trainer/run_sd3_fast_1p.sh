e64842ae80e750923b2776ee1b1de5987efcf7fd
Mike Cheung <zhtmike@gmail.com>
2025-11-26 11:21:01 +0800

    update config

--- examples/flowgrpo_trainer/run_sd3_fast_1p.sh
+++ examples/flowgrpo_trainer/run_sd3_fast_1p.sh
@@ -3,7 +3,7 @@ python3 -m gerl.trainer.main_flowgrpo \
     data.train_files=$HOME/dataset/ocr/train.txt \
     data.val_files=$HOME/dataset/ocr/test.txt \
     data.train_batch_size=8 \
-    data.val_max_samples=16 \
+    data.val_max_samples=32 \
     data.max_prompt_length=128 \
     data.filter_overlong_prompts=False \
     data.data_source=ocr \
@@ -12,7 +12,7 @@ python3 -m gerl.trainer.main_flowgrpo \
     actor_rollout_ref.model.enable_gradient_checkpointing=False \
     actor_rollout_ref.model.lora_rank=32 \
     actor_rollout_ref.model.lora_alpha=64 \
-    actor_rollout_ref.actor.optim.lr=3e-4 \
+    actor_rollout_ref.actor.optim.lr=1e-4 \
     actor_rollout_ref.actor.optim.weight_decay=0.0001 \
     actor_rollout_ref.actor.ppo_mini_batch_size=4 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=8 \
