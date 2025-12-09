7e53ea6f4c651862c7ce51ef8f42c9a5ffe09da6
Mike Cheung <zhtmike@gmail.com>
2025-12-09 11:29:03 +0800

    fix configs

--- examples/flowgrpo_trainer/run_sd3_1p.sh
+++ examples/flowgrpo_trainer/run_sd3_1p.sh
@@ -20,7 +20,6 @@ python3 -m gerl.trainer.main_flowgrpo \
     actor_rollout_ref.actor.kl_loss_coef=0.04 \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
-    actor_rollout_ref.actor.fsdp_config.model_dtype=float16 \
     actor_rollout_ref.actor.fsdp_config.dtype=float16 \
     actor_rollout_ref.actor.fsdp_config.fsdp_size=1 \
     actor_rollout_ref.actor.policy_loss.loss_mode=flow_grpo \
